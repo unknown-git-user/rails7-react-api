c2b449d8b40eb89f227e7f683b0f7ab133d5e2d9
unknown-git-user <emailaddress4myemail@yahoo.com>
2022-07-29 11:30:18 -0400

    rails7 with basic react api

---
+++ bookable/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import axios from 'axios';
+import Books from './components/books';
+import { useEffect, useState } from 'react';
+
+const API_URL = "http://localhost:3000/api/v1/books";
+
+function getApiData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getApiData().then((items) => {
+      if (mounted) {
+        setBooks(items);
+      }
+    });
+
+    return () => (mounted = false);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Books books={books} />
+    </div>
+  );
+}
+
+export default App;
